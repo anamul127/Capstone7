20c2c84157f356f4e5138e2912ca4813af653f4f
Anamul Islam <anamul.islam127@gmail.com>
2018-06-11 12:22:10 -0400

    first commit

---
+++ BonusAPILab/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BonusAPILab.Models;
+
+namespace BonusAPILab.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        NORTHWNDEntities db = new NORTHWNDEntities();
+
+        public List<string> GetProductID()
+        {
+            List<Products> prod = db.Products1.ToList();
+            List<string> product = new List<string>();
+
+            for (int i = 0; i < prod.Count; i++)
+            {
+                product.Add(prod[i].ProductID.ToString());
+                product.Add(prod[i].ProductName);
+            }
+
+            return product;
+        }
+
+        public Products GetProd(int id)
+        {
+            List<Products> prod = db.Products1.ToList();
+            Products p = (from i in db.Products1
+                          where i.ProductID == id
+                          select i).Single();
+            return p;
+
+        }
+    }
+}
